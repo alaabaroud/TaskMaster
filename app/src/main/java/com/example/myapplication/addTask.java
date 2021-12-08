c76263583003ea1621053612de86cdf756e57c40
ALAABAROUD <alaabaroud783@gmail.com>
2021-12-08 21:53:51 +0200

    location

--- app/src/main/java/com/example/myapplication/addTask.java
+++ app/src/main/java/com/example/myapplication/addTask.java
@@ -1,51 +1,69 @@
 package com.example.myapplication;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 //import androidx.room.Room;
 
 import com.amplifyframework.api.graphql.model.ModelMutation;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.TaskModel;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class addTask extends AppCompatActivity {
+
+
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
+
+
+
 //        TextView showSubmission=findViewById(R.id.submitted);
 //        showSubmission.setText("Submission");
 
 
+        EditText titleName = findViewById(R.id.titleName);
+        EditText bodyName = findViewById(R.id.bodyName);
+        EditText statusName = findViewById(R.id.statusName);
 
 
-
-        EditText titleName=findViewById(R.id.titleName);
-        EditText bodyName=findViewById(R.id.bodyName);
-        EditText statusName=findViewById(R.id.statusName);
-
-
-        Button showSubmission=findViewById(R.id.addTask);
+        Button showSubmission = findViewById(R.id.addTask);
         showSubmission.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 
-                String getTitle=titleName.getText().toString();
-                String getBody=bodyName.getText().toString();
-                String getStatus=statusName.getText().toString();
-
-
+                String getTitle = titleName.getText().toString();
+                String getBody = bodyName.getText().toString();
+                String getStatus = statusName.getText().toString();
 
 
                 Intent intent = new Intent(addTask.this, MainActivity.class);
@@ -54,7 +72,7 @@ public class addTask extends AppCompatActivity {
 //                    TaskModel1 taskModel = new TaskModel1(getTitle, getBody, getStatus);
 
                 try {
-                    TaskModel taskModel=TaskModel.builder().title(getTitle).body(getBody).state(getStatus).build();
+                    TaskModel taskModel = TaskModel.builder().title(getTitle).body(getBody).state(getStatus).build();
 
                     Amplify.API.mutate(
                             ModelMutation.create(taskModel),
@@ -62,8 +80,7 @@ public class addTask extends AppCompatActivity {
                             error -> Log.e("MyAmplifyApp", "Create failed", error)
                     );
 
-                }
-                catch(NullPointerException nullPointerException){
+                } catch (NullPointerException nullPointerException) {
                     System.out.println("this is an error");
 
                 }
@@ -72,10 +89,9 @@ public class addTask extends AppCompatActivity {
                 startActivity(intent);
 
 
-
-
-
             }
         });
     }
+
+
 }
