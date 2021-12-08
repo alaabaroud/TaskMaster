7f66d5bcb95a4be77e505cfb8021e68e2e93e83f
ALAABAROUD <alaabaroud783@gmail.com>
2021-12-08 21:56:59 +0200

    location

--- app/src/main/java/com/example/myapplication/Location.java
+++ app/src/main/java/com/example/myapplication/Location.java
@@ -35,9 +35,10 @@ public class Location extends AppCompatActivity {
         locationbtn = findViewById(R.id.locationbtn);
         Cityloc = findViewById(R.id.CityLoc);
         lan = findViewById(R.id.lan);
+
         lng = findViewById(R.id.lng);
 
-        
+
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         locationbtn.setOnClickListener(new View.OnClickListener() {
@@ -60,11 +61,7 @@ public class Location extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             return;
         }
         fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<android.location.Location>() {
